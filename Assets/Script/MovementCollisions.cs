aa28416c25f1b6464b2d9f43b450221db5d45e9f
KaJoseh <48393210+KaJoseh@users.noreply.github.com>
2020-09-07 13:27:10 -0500

    Platformer Behaviours
    Movement Scripts finished.

---
+++ Assets/Script/MovementCollisions.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementCollisions : MonoBehaviour
+{
+    public LayerMask groundLayer;
+
+    [Space]
+
+    [Range(0.1f, 1.0f)]
+    public float radius = 0.2f;
+    [Range(0.1f, 1.0f)]
+    public float rightSideOff;
+    [Range(0.1f, 1.0f)]
+    public float leftSideOff;
+    [Range(0.1f, 1.0f)]
+    public float downSideOff;
+
+    [Space]
+
+    public bool onGround;
+    public bool onWall;
+    public bool onRightWall;
+    public bool onLeftWall;
+    //public int wallJumpSide;
+
+    // Update is called once per frame
+    void Update()
+    {
+        onGround = Physics2D.OverlapCircle(new Vector2(transform.position.x, transform.position.y - downSideOff), radius, groundLayer);
+        onWall = Physics2D.OverlapCircle(new Vector2(transform.position.x + rightSideOff, transform.position.y), radius, groundLayer) 
+            || Physics2D.OverlapCircle(new Vector2(transform.position.x - leftSideOff, transform.position.y), radius, groundLayer);
+
+
+        onRightWall = Physics2D.OverlapCircle(new Vector2(transform.position.x + rightSideOff, transform.position.y), radius, groundLayer);
+        onLeftWall = Physics2D.OverlapCircle(new Vector2(transform.position.x - leftSideOff, transform.position.y), radius, groundLayer);
+
+        //wallJumpSide = onRightWall ? -1 : 1;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawWireSphere(new Vector3(transform.position.x + rightSideOff, transform.position.y), radius); //right gizmo
+        Gizmos.DrawWireSphere(new Vector3(transform.position.x - leftSideOff, transform.position.y), radius); //left gizmo
+        Gizmos.DrawWireSphere(new Vector3(transform.position.x, transform.position.y - downSideOff), radius); //vertical gizmo
+    }
+}
--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -3,29 +3,82 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(BetterJumping))]
+[RequireComponent(typeof(MovementCollisions))]
 public class PlayerMovement : MonoBehaviour
 {
+
     public KeyCode leftKey, rightKey, jumpKey;
+
+    [Space]
+
     public float speed;
     public float jumpForce;
-    Rigidbody2D rb;
+    [Header("Time to fall from a wall")]
+    public float fallWallCounter;
+    private float counter;
 
-    private bool canJump;
-    private bool inAir;
-    private bool onWall;    
+    private Rigidbody2D rb;
+    private MovementCollisions mColl;
+    private bool wallJumping;
 
+   
     // Start is called before the first frame update
     void Start()
     {
+        counter = fallWallCounter;
         rb = gameObject.GetComponent<Rigidbody2D>();
+        mColl = GetComponent<MovementCollisions>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Walk(leftKey, rightKey);
-        if (Input.GetKey(jumpKey) && canJump)
+        if((!mColl.onWall || mColl.onGround) && !wallJumping)
+            Walk(leftKey, rightKey);
+        if (Input.GetKey(jumpKey) && mColl.onGround)
             Jump();
+
+        if((!mColl.onWall && !mColl.onGround) || mColl.onGround)
+        {
+            rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+            counter = fallWallCounter;
+        }
+
+
+        if (mColl.onWall && !mColl.onGround)
+        {
+            rb.constraints = RigidbodyConstraints2D.FreezeAll;
+
+            if (counter <= 0)
+            {
+                if (mColl.onWall && mColl.onRightWall)
+                {
+                    JumpingFromWall();
+                    rb.velocity = new Vector2(-speed, rb.velocity.y);
+                }
+                else if (mColl.onWall && mColl.onLeftWall)
+                {
+                    JumpingFromWall();
+                    rb.velocity = new Vector2(speed, rb.velocity.y);
+                }
+            }
+            else if (counter >= 0)
+                counter -= Time.deltaTime * 2;
+
+
+            if (Input.GetKey(jumpKey))
+            {
+                WallJump();
+            }
+            else if (Input.GetKey(rightKey) && !mColl.onRightWall)
+            {
+                WallJump();
+            }
+            else if (Input.GetKey(leftKey) && mColl.onRightWall)
+            {
+                WallJump();
+            }
+        }
     }
 
     #region Walk&Jump functions
@@ -44,18 +97,34 @@ public class PlayerMovement : MonoBehaviour
     {
         rb.velocity = new Vector2(rb.velocity.x, 0);
         rb.velocity += Vector2.up * jumpForce;
-        inAir = true;
-        canJump = false;
+    }
+
+    private void WallJump()
+    {
+        JumpingFromWall();
+        rb.velocity = new Vector2(rb.velocity.x, 0);
+        if (mColl.onRightWall)
+        {
+            rb.velocity = new Vector2(-speed, jumpForce);
+        }
+        else if (mColl.onLeftWall)
+        {
+            rb.velocity = new Vector2(speed, jumpForce);
+        }
+    }
+
+    void JumpingFromWall()
+    {
+        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+        wallJumping = true;
+        StartCoroutine(SetWalljumpingFalse());
     }
 
     #endregion
 
-    private void OnCollisionEnter2D(Collision2D c)
+    IEnumerator SetWalljumpingFalse()
     {
-        if (c.gameObject.tag.Equals("Floor") && !canJump)
-        {
-            inAir = false;
-            canJump = true;
-        }
+        yield return new WaitForSeconds(0.09f);
+        wallJumping = false;
     }
 }
