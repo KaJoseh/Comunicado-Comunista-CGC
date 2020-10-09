3cd04c552acde9f0d65519260cbaae9dfb124881
Kaleth Cristancho <48393210+KaJoseh@users.noreply.github.com>
2020-10-09 03:00:11 -0500

    Basic PickUp system added.
    Also some sprites.

---
+++ Assets/Scripts/Pick-ups/PickUpsController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpsController : MonoBehaviour
+{
+
+    public GameObject pickUp;
+    public int pickUpsAmount;
+
+    GameObject[] pickUpsLocations;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GeneratePickUps();
+    }
+
+
+    void GeneratePickUps()
+    {
+        pickUpsLocations = GameObject.FindGameObjectsWithTag("PickUpLocation");
+
+        for(int i = 0; i < pickUpsAmount; i++)
+        {
+            shuffleArray(pickUpsLocations);
+            Instantiate(pickUp, pickUpsLocations[0].transform.position, pickUpsLocations[0].transform.rotation);
+        }
+
+    }
+
+    void shuffleArray(GameObject[] array)
+    {
+        // Knuth shuffle algorithm :: courtesy of Wikipedia :)
+        for (int t = 0; t < array.Length; t++)
+        {
+            GameObject tmp = array[t];
+            int r = Random.Range(t, array.Length);
+            array[t] = array[r];
+            array[r] = tmp;
+        }
+    }
+}
